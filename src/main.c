1b6b7ed04492f8954e2a49478bbd46db4a24fff8
Addison Mink <addisonmink@Addisons-MacBook-Pro.local>
2024-03-15 23:01:18 -0500

    Make enemy mockup.

--- src/main.c
+++ src/main.c
@@ -6,25 +6,47 @@
 int main(void)
 {
     InitWindow(800, 448, "Template-5.0.0");
-    UI *ui = UIAlloc(10);
+    UI *ui = UIAlloc(100);
     Texture2D texture = LoadTexture("assets/nineslice.png");
+    Texture2D goblinTexture = LoadTexture("assets/goblin_32.png");
     Font font = LoadFont("assets/kongtext.ttf");
+
+    int size = 128;
 
     while (!WindowShouldClose())
     {
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            size = (size == 128) ? 64 : 128;
+        }
+
         BeginDrawing();
 
-        ClearBackground(BLACK);        
+        ClearBackground(BLACK);
 
         UIReset(ui);
         {
+            UIShim(ui, 800, 448);
+            UIAlign(ui, ALIGN_H_CENTER, ALIGN_V_CENTER);
+            UIRow(ui, 20);
+
             UIOverlay(ui);
-            UINineSlice(ui, texture, 1, 4, 200, 100, WHITE);
-            UIOffset(ui, (Vector2){10,10});
-            UILabel(ui, font, "Hello, World!", 12, WHITE);
+            UIShim(ui,128,128);
+            UIAlign(ui, ALIGN_H_CENTER, ALIGN_V_CENTER);
+            UISprite(ui, goblinTexture, size, size, BLUE);
+
+            UINineSlice(ui,texture,1,4,128,28,WHITE);
+
+            UIAlignH(ui,ALIGN_H_CENTER);
+            UIOffset(ui,(Vector2){0,8});
+            UILabel(ui, font, "Cleaver", 12, RAYWHITE);
             UIOverlayEnd(ui);
+            
+            UISprite(ui, goblinTexture, 128, 128, WHITE);
+            UISprite(ui, goblinTexture, 128, 128, WHITE);
+            UIRowEnd(ui);
         }
-        UIDraw(ui, (Vector2){100, 100});
+        UIDraw(ui, (Vector2){0, 0});
 
         EndDrawing();
     }
@@ -33,6 +55,7 @@ int main(void)
 
     UIFree(ui);
     UnloadTexture(texture);
+    UnloadTexture(goblinTexture);
     UnloadFont(font);
     return 0;
 }
