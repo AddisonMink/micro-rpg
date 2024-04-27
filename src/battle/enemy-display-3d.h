963fcae2038736e8d7a4189c5fd13b9e60a6993d
Addison Mink <addisonmink@Addisons-MacBook-Pro.local>
2024-04-26 19:10:33 -0500

    Revert "Draw enemies in 3d."
    This reverts commit 4535baf67a00514e0c42b6370aa61cafb9b5eff3.

--- src/battle/enemy-display-3d.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "battle/event.h"
-#include "battle/combatant.h"
-#include "common/common-ui.h"
-
-void EnemyDisplay3D_Draw(const Camera3D *camera, const Combatant combatants[MAX_COMBATANTS], const Event *event);
